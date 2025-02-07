870ee265c167ffa831de02e9545628a800a173d8
vijji <vijayashree.cse@anurag.ac.in>
2025-02-07 14:41:54 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,8 @@
+let message ="hello";
+let name="sameer";
+const PI = 3.14;
+function greet(name){
+    return `hello,${name}!`;
+}
+console.log(greet("sameer"));
+
---
+++ name.js
@@ -0,0 +1,2 @@
+let name="rajesh";
+console.log(name);
